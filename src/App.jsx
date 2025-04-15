e022e3e24a0808d51b4d896c1fdc508d3af32425
Vishwas Joshi <vish@Vishwass-MacBook-Pro.local>
2025-04-16 09:03:09 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
